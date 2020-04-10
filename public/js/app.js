9343943348eeb8eebfa2d97f35f19432860a6d1e
Marko <89n.marko@gmail.com>
2020-04-10 12:59:23 +0200

    setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -35,87 +35,17 @@ const fetching = (location) => {
 }
 
 
-
-
 const searchAddress = (new URLSearchParams(window.location.search)).get('address')
 
-
-
-
-
 if (searchAddress) {
-    //const location = search.value
-
-    fetching(searchAddress)
-
-    // messageOne.textContent = 'Loading...'
-    // messageTwo.textContent = ''
-
-    // fetch('/weather?address=' + searchAddress).then((response) => {
-
-    //     console.log(response)
-
-    //     response.json().then((data) => {
-
-    //         console.log(data)
-
-    //         if (data.error) {
-    //             messageOne.textContent = data.error
-    //         } else {
-    //             messageOne.textContent = data.location
-    //             messageTwo.textContent = data.forecast
-
-
-    //             console.log('Marko 2')
-
-    //             //let stateObj = { id: "100" };
-    //             window.history.replaceState(null,
-    //                 "", "?address=" + searchAddress);
-
-
-
-    //         }
-    //     })
-    // })
+        fetching(searchAddress)
 }
-
 
 weatherForm.addEventListener('click', () => {
     //e.preventDefault()
 
     const search = document.querySelector('input')
-
     fetching(search.value)
-
-    // messageOne.textContent = 'Loading...'
-    // messageTwo.textContent = ''
-
-    // fetch('/weather?address=' + location).then((response) => {
-
-    //     console.log(response)
-
-    //     response.json().then((data) => {
-
-    //         console.log(data)
-
-    //         if (data.error) {
-    //             messageOne.textContent = data.error
-    //         } else {
-    //             messageOne.textContent = data.location
-    //             messageTwo.textContent = data.forecast
-
-
-    //             console.log('Marko 2')
-
-    //             //let stateObj = { id: "100" };
-    //             window.history.replaceState(null,
-    //                 "", "?address=" + location);
-
-
-
-    //         }
-    //     })
-    // })
 })
 
 
--- src/app.js
+++ src/app.js
@@ -7,6 +7,9 @@ const forecast = require('./utils/forecast')
 
 const app = express()
 
+// this is the port number we listen to. For Heroku we use process.env.PORT variable and locally we use 3000
+const port = process.env.PORT || 3000
+
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
@@ -110,7 +113,7 @@ app.get('*', (req, res) => {
 
 
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000')
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
 })
 
