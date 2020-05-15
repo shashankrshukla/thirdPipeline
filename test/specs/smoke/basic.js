c3b82c15df0593628ed2048f3329b4be2d810ebf
Shashank <Shashankshukla1988@gmail.com>
2020-05-15 23:52:30 +0530

    third commit of the project, hoping jenkins to trigger by the push

--- test/specs/smoke/basic.js
+++ test/specs/smoke/basic.js
@@ -4,15 +4,11 @@ const assert = require('assert')
 describe('Testing the title of the Weather Webpage', () => {
     it('should have the right title', () => {
         browser.url('/')
-        const title = browser.getTitle()
-        assert.strictEqual(title, 'Weather');
-        browser.setTimeout({
-            'script': 60000
-        })
-        browser.executeAsync((done) => {
-            console.log('this should not fail')
-            setTimeout(done, 25000)
-        })
+        const butt = $('/html/body/div/button');
+        butt.click();
+        // const title = browser.getTitle()
+        // assert.strictEqual(title, 'Weather');
+
     })
 })
 //end
