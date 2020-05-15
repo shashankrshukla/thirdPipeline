18877a0420890bd0d15229bc9b07768e6f4ddfbd
Shashank <Shashankshukla1988@gmail.com>
2020-05-16 00:56:11 +0530

    fourth commit of the project, npm is now getting installed after downgrading node from 14.x.x to 12.x.x

--- test/specs/smoke/basic.js
+++ test/specs/smoke/basic.js
@@ -5,9 +5,23 @@ describe('Testing the title of the Weather Webpage', () => {
     it('should have the right title', () => {
         browser.url('/')
         const butt = $('/html/body/div/button');
+        browser.setTimeout({
+            'script': 60000
+        })
+        browser.executeAsync((done) => {
+            console.log('this should not fail')
+            setTimeout(done, 5000)
+        })
         butt.click();
-        // const title = browser.getTitle()
-        // assert.strictEqual(title, 'Weather');
+        browser.setTimeout({
+            'script': 60000
+        })
+        browser.executeAsync((done) => {
+            console.log('this should not fail')
+            setTimeout(done, 5000)
+        })
+        const title = browser.getTitle()
+        assert.strictEqual(title, 'Weather');
 
     })
 })
